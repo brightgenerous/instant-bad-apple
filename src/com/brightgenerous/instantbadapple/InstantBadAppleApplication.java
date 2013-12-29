34c199970f6413014901aef449a58a6f5212a93d
brightgenerous <katou.akihiro@gmail.com>
2013-12-29 20:02:57 +0900

    Bad Apple らしくした

--- src/com/brightgenerous/instantbadapple/InstantBadAppleApplication.java
+++ src/com/brightgenerous/instantbadapple/InstantBadAppleApplication.java
@@ -12,21 +12,25 @@ import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
+import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
 import javafx.scene.SceneBuilder;
 import javafx.scene.control.Label;
 import javafx.scene.control.LabelBuilder;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.FlowPaneBuilder;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
+import javafx.scene.media.MediaMarkerEvent;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.media.MediaViewBuilder;
 import javafx.stage.Stage;
 import javafx.stage.StageBuilder;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public class InstantBadAppleApplication extends Application {
 
@@ -64,11 +68,12 @@ public class InstantBadAppleApplication extends Application {
             Label label = LabelBuilder.create().text(text).build();
             FlowPane root = FlowPaneBuilder.create().children(label).alignment(Pos.CENTER).build();
             Scene scene = SceneBuilder.create().root(root).build();
-            StageBuilder.create().scene(scene).width(300).height(240).title("Instant Bad Apple - Error").applyTo(stage);
+            StageBuilder.create().scene(scene).width(300).height(240)
+                    .title("Instant Bad Apple - Error").applyTo(stage);
         } else {
-            MediaView mediaView = MediaViewBuilder.create().mediaPlayer(player).build();
+            final MediaView mediaView = MediaViewBuilder.create().mediaPlayer(player).build();
 
-            Pane root = new Pane() {
+            final Pane root = new Pane() {
 
                 @Override
                 protected void layoutChildren() {
@@ -91,6 +96,51 @@ public class InstantBadAppleApplication extends Application {
             stage.setFullScreen(true);
             stage.setOpacity(0.7);
 
+            final ImageCursor iconW = new ImageCursor(new Image(
+                    InstantBadAppleApplication.class.getResourceAsStream("icon_w.png")), 20, 20);
+            final ImageCursor iconB = new ImageCursor(new Image(
+                    InstantBadAppleApplication.class.getResourceAsStream("icon_b.png")), 20, 20);
+
+            root.setCursor(iconB);
+            root.setStyle("-fx-background-color:black;");
+            player.getMedia().getMarkers().put("01-white", Duration.seconds(1.40));
+            player.getMedia().getMarkers().put("02-black", Duration.seconds(14.76));
+            player.getMedia().getMarkers().put("03-white", Duration.seconds(27.33));
+            player.getMedia().getMarkers().put("04-black", Duration.seconds(42.06));
+            player.getMedia().getMarkers().put("05-white", Duration.seconds(56.16));
+            player.getMedia().getMarkers().put("06-black", Duration.seconds(57.96));
+            player.getMedia().getMarkers().put("07-white", Duration.seconds(74.53));
+            player.getMedia().getMarkers().put("08-black", Duration.seconds(83.56));
+
+            player.getMedia().getMarkers().put("09-white", Duration.seconds(91.40));
+            player.getMedia().getMarkers().put("10-black", Duration.seconds(94.50));
+            player.getMedia().getMarkers().put("11-white", Duration.seconds(110.73));
+            player.getMedia().getMarkers().put("12-black", Duration.seconds(120.80));
+            player.getMedia().getMarkers().put("13-white", Duration.seconds(121.66));
+            player.getMedia().getMarkers().put("14-black", Duration.seconds(122.53));
+
+            player.getMedia().getMarkers().put("15-white", Duration.seconds(126.50));
+            player.getMedia().getMarkers().put("16-black", Duration.seconds(142.03));
+            player.getMedia().getMarkers().put("17-white", Duration.seconds(154.43));
+            player.getMedia().getMarkers().put("18-black", Duration.seconds(181.03));
+            player.getMedia().getMarkers().put("19-white", Duration.seconds(204.60));
+            player.getMedia().getMarkers().put("20-black", Duration.seconds(209.93));
+            player.getMedia().getMarkers().put("21-white", Duration.seconds(215.60));
+            player.getMedia().getMarkers().put("22-black", Duration.seconds(217.13));
+
+            player.setOnMarker(new EventHandler<MediaMarkerEvent>() {
+
+                @Override
+                public void handle(MediaMarkerEvent event) {
+                    if (event.getMarker().getKey().endsWith("white")) {
+                        root.styleProperty().set("-fx-background-color:white;");
+                        root.cursorProperty().setValue(iconW);
+                    } else {
+                        root.styleProperty().set("-fx-background-color:black;");
+                        root.cursorProperty().setValue(iconB);
+                    }
+                }
+            });
             player.setOnReady(new Runnable() {
 
                 @Override
